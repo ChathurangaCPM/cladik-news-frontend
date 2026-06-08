ff085206c8d625d5a4917934e80573d20402b6cd
ChathurangaCPM <chathurangaonline.msg@gmail.com>
2026-06-08 18:25:55 +0530

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     "",
     "/how-it-works",
     "/pricing",
-    "/changelog",
+    // "/changelog",
     "/login",
     "/signup",
   ];
